8a7d29b72aa9c3e6449d141dfa225cd06eb38533
wujunyan <wujunyan>
2016-12-30 16:55:07 +0800

    [modify]add final keyword to the static field in the static inner class

--- src/main/java/com/temp/designPatterns/singleton/Singleton.java
+++ src/main/java/com/temp/designPatterns/singleton/Singleton.java
@@ -6,7 +6,7 @@ public class Singleton {
 	private Singleton() {}
 
 	private static class SingletonHolder {
-		private static Singleton instance = new Singleton();
+		private static final Singleton instance = new Singleton();
 	}
 	
 	public static Singleton getInstance() {
