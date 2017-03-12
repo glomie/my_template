875387023af7e1532985429e68c9c8e08dbcdc72
junyan <junyan@DESKTOP-1ICR7KA>
2017-03-12 21:00:32 +0800

    [add]add some tests

---
+++ src/test/java/com/example/test/juc/JoinTest.java
@@ -0,0 +1,30 @@
+package com.example.test.juc;
+
+public class JoinTest {
+
+	public static void main(String[] args) {
+		Thread threadB = new ThreadB();
+		threadB.start();
+		try {
+			threadB.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " finish");
+	}
+}
+
+class ThreadB extends Thread {
+	
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " start");
+		try {
+			System.out.println("sleeping 1000ms");
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " finish");
+	}
+}
