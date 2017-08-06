0897324c8631c534cdc7e1765f74eb635ec6ff25
junyan <junyan@DESKTOP-1ICR7KA>
2017-08-07 00:01:58 +0800

    [add]some sort tests

--- src/test/java/com/example/test/sort/Client.java
+++ src/test/java/com/example/test/sort/Client.java
@@ -7,7 +7,7 @@ public class Client {
 	private static int[] data = {49,38,65,97,76,13,27,49,55,04};
 	
 	public static void main(String[] args) {
-		ISort sort = new BubbleSort();
+		ISort sort = new HeapSort();
 		sort.sort(data);
 		System.out.println(Arrays.toString(data));
 	}
