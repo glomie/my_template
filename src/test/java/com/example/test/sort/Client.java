19a932a0942ded8f96e31ab67c1c50e612483d41
junyan <junyan@DESKTOP-1ICR7KA>
2017-08-08 22:18:33 +0800

    [add]add radix sort

--- src/test/java/com/example/test/sort/Client.java
+++ src/test/java/com/example/test/sort/Client.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Client {
 
-	private static int[] data = {49,38,65,97,76,13,27,49,55,04};
+	private static int[] data = {49,38,65,97,76,13,27,49,55,104};
 	
 	public static void main(String[] args) {
-		ISort sort = new MergeSort();
+		ISort sort = new RadixSort();
 		sort.sort(data);
 		System.out.println(Arrays.toString(data));
 	}
